okuhara/edax-reversi-AVX

Omit unpack from get_edge_stability

src/bit.c
@@ -6,7 +6,7 @@
  * a macro needs to be defined to chose between different flavors of the
  * algorithm.
  *
- * @date 1998 - 2021
+ * @date 1998 - 2022
  * @author Richard Delorme
  * @version 4.5
  */
@@ -83,7 +83,7 @@ int bit_count(unsigned long long b)
 // 2% faster than SWAR bit_count for 32 & 64 non-POPCOUNT build
 unsigned char PopCnt16[1 << 16];
 
-static int bit_count_32(unsigned int b)
+static int bit_count_32_SWAR(unsigned int b)
 {
 	b = b - ((b >> 1) & 0x55555555);
 	b = ((b >> 2) & 0x333333333) + (b & 0x33333333);
@@ -129,7 +129,7 @@ void bit_init(void)
 
 #ifndef POPCOUNT
 	for (n = 0; n < (1 << 16); ++n)
-		PopCnt16[n] = bit_count_32(n);
+		PopCnt16[n] = bit_count_32_SWAR(n);
 #endif
 
 #if (defined(USE_GAS_MMX) || defined(USE_MSVC_X86)) && !defined(hasSSE2)

src/board.h
@@ -84,7 +84,7 @@ unsigned long long get_moves_sse(unsigned long long, unsigned long long);
 
 extern unsigned char edge_stability[256 * 256];
 
-#if defined(__BMI2__) && !defined(bdver4) && !defined(znver1) && !defined(znver2) // pdep is slow on AMD before Zen3
+#if 0 // defined(__BMI2__) && !defined(bdver4) && !defined(znver1) && !defined(znver2) // pdep is slow on AMD before Zen3
 #define	unpackA1A8(x)	_pdep_u64((x), 0x0101010101010101)
 #define	unpackH1H8(x)	_pdep_u64((x), 0x8080808080808080)
 #else

okuhara/edax-reversi-AVX-v446mod2

src/bit.c
@@ -76,7 +76,7 @@ int bit_count(unsigned long long b)
 // 2% faster than SWAR bit_count for 32 & 64 non-POPCOUNT build
 unsigned char PopCnt16[1 << 16];
 
-static int bit_count_32(unsigned int b)
+static int bit_count_32_SWAR(unsigned int b)
 {
 	b = b - ((b >> 1) & 0x55555555);
 	b = ((b >> 2) & 0x333333333) + (b & 0x33333333);
@@ -101,7 +101,7 @@ void bit_init(void)
 
 #ifndef POPCOUNT
 	for (i = 0; i < (1 << 16); ++i)
-		PopCnt16[i] = bit_count_32(i);
+		PopCnt16[i] = bit_count_32_SWAR(i);
 #endif
 #if (defined(USE_GAS_MMX) || defined(USE_MSVC_X86)) && !defined(hasSSE2)
 	init_mmx();

src/board.h
@@ -85,7 +85,7 @@ int get_stability_sse(const unsigned long long P, const unsigned long long O);
 extern unsigned char edge_stability[256 * 256];
 extern unsigned long long A1_A8[256];
 
-#if defined(__BMI2__) && !defined(bdver4) && !defined(znver1) && !defined(znver2) // pdep is slow on AMD before Zen3
+#if 0 // defined(__BMI2__) && !defined(__bdver4__) && !defined(__znver1__) && !defined(__znver2__) // pdep is slow on AMD before Zen3
 	#define	unpackA1A8(x)	_pdep_u64((x), 0x0101010101010101)
 	#define	unpackH1H8(x)	_pdep_u64((x), 0x8080808080808080)
 #else